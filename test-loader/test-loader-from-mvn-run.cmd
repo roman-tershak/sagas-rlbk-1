5ba64bb56ed3af9782d1c81c25964ff02b299eeb
Roman Tershak <romantershak@rewconsultingservices.com>
2020-03-26 22:18:56 +0200

    some changes

--- test-loader/test-loader-from-mvn-run.cmd
+++ test-loader/test-loader-from-mvn-run.cmd
@@ -1,5 +1,5 @@
 docker run --name test-loader ^
     -ti --rm ^
     --network docker_vnet-saga-w/rollbacks-1 ^
-    -e CONCURRENCY=10 -e HOLD_FOR=300s -e RAMP_UP=10s ^
+    -e CONCURRENCY=10 -e HOLD_FOR=900s -e RAMP_UP=10s ^
     saga-tests-rlbk-1/test-loader:3.0-RLBK-NSTR-SEQEV
